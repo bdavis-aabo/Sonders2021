2c8619f99828faa5a4b2486bdd80b182ef8a7403
bdavis-aabo <bdavis@artandbusinessone.com>
2022-07-25 15:41:55 -0600

    Updating the Sonders Forum page to include survey #6 embed code.

--- wp-content/themes/sonders2021/page-templates/page-forum.php
+++ wp-content/themes/sonders2021/page-templates/page-forum.php
@@ -27,6 +27,10 @@
 				<script>
 					(function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=e.getElementsByTagName(s),a=o[o.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgdxUBLG_2Bzm7As7ultFwnYXRHz7IUtWNm6b7jxZoshlm5Q.js",a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");
 				</script>
+			<?php elseif(is_page('sonders-forum-survey-6')): ?>
+				<script>
+					(function(t,e,s,n){var o,a,c;t.SMCX=t.SMCX||[],e.getElementById(n)||(o=e.getElementsByTagName(s),a=o[o.length-1],c=e.createElement(s),c.type="text/javascript",c.async=!0,c.id=n,c.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd_2F1P_2B59EX_2FxfFwX4gkVO_2F8rmvWdsxQecRelBh7e7YwFX.js",a.parentNode.insertBefore(c,a))})(window,document,"script","smcx-sdk");
+				</script>
 				<?php endif; ?>
         </div>
       </div>
