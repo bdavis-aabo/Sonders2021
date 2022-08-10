17d046d4463ea886172431ad3e5d89163d484e5b
bdavis-aabo <bdavis@artandbusinessone.com>
2022-08-10 10:25:24 -0600

    Creating the Custom Homes section on the HOMES page.

--- wp-content/themes/sonders2021/page-templates/page-homes.php
+++ wp-content/themes/sonders2021/page-templates/page-homes.php
@@ -20,5 +20,34 @@
 
     <?php if(is_page('homes')): get_template_part('homes/homes-builders'); endif; ?>
 
+		<?php if(get_field('custom_homes_content') != ''): $_lotImage = get_field('custom_homes_lots'); ?>
+		<section class="page-section custom-homes-section tan-bg">
+			<div class="custom-homes-container">
+				<figure class="home-lots">
+					<img src="<?php echo $_lotImage['url'] ?>" alt="Sonders Custom Homes Lots" class="img-fluid" />
+				</figure>
+				<article class="home-lots-content">
+					<div class="lot-content-container">
+						<?php echo get_field('custom_homes_content') ?>
+					</div>
+					<?php if(have_rows('custom_homes_realtors')): ?>
+					<div class="custom-realtors">
+						<?php while(have_rows('custom_homes_realtors')): the_row(); $_logo = get_sub_field('logo'); ?>
+							<article class="realtor">
+								<figure class="logo">
+									<img src="<?php echo $_logo['url'] ?>" alt="Realtor Logo" class="img-fluid" />
+								</figure>
+								<div class="realtor-contact">
+									<?php echo get_sub_field('contact_info') ?>
+								</div>
+							</article>
+						<?php endwhile; ?>
+					</div>
+					<?php endif; ?>
+				</article>
+			</div>
+		</section>
+		<?php endif; ?>
+
   <?php endwhile; endif; ?>
 <?php get_footer(); ?>
